df4c3c23b6bf2e750cd8376598c79af6dd3e68ed
korkem <73012254+kkorkemm@users.noreply.github.com>
2021-10-13 13:08:18 +0600

    locations controller

---
+++ Session1Api/Controllers/DepartmentLocationsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Session1Api.Base;
+
+namespace Session1Api.Controllers
+{
+    public class DepartmentLocationsController : ApiController
+    {
+        private KazanNeftSession1DBEntities db = new KazanNeftSession1DBEntities();
+
+        // GET: api/DepartmentLocations
+        [ResponseType(typeof(List<DepartmentLocations>))]
+        public IHttpActionResult GetDepartmentLocations()
+        {
+            return Ok(db.DepartmentLocations);
+        }
+
+        // GET: api/DepartmentLocations/5
+        [ResponseType(typeof(DepartmentLocations))]
+        public IHttpActionResult GetDepartmentLocations(long id)
+        {
+            DepartmentLocations departmentLocations = db.DepartmentLocations.Find(id);
+            if (departmentLocations == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(departmentLocations);
+        }
+    }
+}
---
+++ Session1Api/Controllers/LocationsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Session1Api.Base;
+
+namespace Session1Api.Controllers
+{
+    public class LocationsController : ApiController
+    {
+        private KazanNeftSession1DBEntities db = new KazanNeftSession1DBEntities();
+
+        // GET: api/Locations
+        [ResponseType(typeof(List<Locations>))]
+        public IHttpActionResult GetLocations()
+        {
+            return Ok(db.Locations);
+        }
+
+        // GET: api/Locations/5
+        [ResponseType(typeof(Locations))]
+        public IHttpActionResult GetLocations(long id)
+        {
+            Locations locations = db.Locations.Find(id);
+            if (locations == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(locations);
+        }
+    }
+}
